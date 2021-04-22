bbd530907537b71694e49ad393f53e583fb362b6
toshi-bp <yttoshi-bp@outlook.jp>
2021-04-22 13:37:10 +0900

    ファイルのアップロードはできた

--- AdJobApp/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class UpoadFileForm(forms.Form):
-    title = forms.CharField(max_length=200)
-    file = forms.FileField()
--- AdJobApp/urls.py
+++ AdJobApp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+import file_upload.views as file_upload
 
 urlpatterns = [
+    path('success/url', file_upload.success),
+    path('file_upload/', include('file_upload.urls')),
     path('admin/', admin.site.urls),
 ]
